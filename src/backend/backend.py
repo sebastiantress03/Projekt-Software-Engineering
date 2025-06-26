7fb4e4aef7899d4cbba2616789c795d3adad1ecd
sebastiantress03 <SebastianTress@t-online.de>
2025-06-26 13:25:32 +0200

    add delete in allow_methods

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -19,7 +19,7 @@ api.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET","POST","PUT"],
+    allow_methods=["GET","POST","PUT","DELETE"],
     allow_headers=["*"], 
 )
 
